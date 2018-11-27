fb6d733573489fc56c3877047e102af92feb0b5e
Jason Liu <jason.liu@oocl.com>
2018-11-27 16:43:10 +0800

    return list as JSON

---
+++ src/main/java/com/tw/apistackbase/controller/Employee.java
@@ -0,0 +1,46 @@
+package com.tw.apistackbase.controller;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Employee(int id, String name, int age, String gender){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -1,20 +1,37 @@
 package com.tw.apistackbase.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
 public class EmployeeResource {
     private final Logger log = Logger.getLogger(this.getClass().getName());
-
-    @RequestMapping(value = "/employees")
-    public ResponseEntity<String> getAll() {
-
-        return ResponseEntity.ok("Hello:");
+    private Employee e1 = new Employee(0, "Xiaoming", 20, "M");
+    private Employee e2 = new Employee(1,"Xiaohong", 19, "F");
+    private Employee e3 = new Employee(2, "Xiaozhi", 15, "M");
+    private Employee e4 = new Employee(3, "Xiaogang", 16, "M");
+    @RequestMapping(value = "/employees", produces = "application/json")
+    public List<Employee> listAllEmployee(){
+        List<Employee> allEmployee = new ArrayList<>();
+        allEmployee.add(e1);
+        allEmployee.add(e2);
+        allEmployee.add(e3);
+        return allEmployee;
     }
+    /*public ResponseEntity<String> getAll() {
+
+        return ResponseEntity.ok(Employee);
+        //return ResponseEntity.ok();
+    }
+    */
 }
+
